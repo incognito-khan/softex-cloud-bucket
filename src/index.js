b99bc3535d6a9797b5ce2e554790f75c5a2dcc62
incognito-khan <abnadeem780@gmail.com>
2025-11-22 17:30:37 +0500

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors"; 
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
+app.use(cors()); 
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
